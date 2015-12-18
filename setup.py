5b9752ffa46f0ad20b324ef46aac14b9b5650f63
Ilya Moroz <flamewow@yandex.ru>
2015-12-18 23:52:35 +0200

    prepare to publish on PYPI

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     author = 'Moroz Ilya',
     author_email = 'flamewowilia@gmail.com',
-    description = 'Modification of python library for interacting with the Apple Push Notification Service',
+    description = 'Modification of PyAPNs python library for interacting with the Apple Push Notification Service',
     download_url = 'https://github.com/flamewow/PyAPNs.git',
     license = 'unlicense.org',
     name = 'apns_py2py3',
