b5c853542ed25ccf17daa603adc41ef7c4870bdf
Ilya Moroz <flamewow@yandex.ru>
2015-12-18 11:13:44 +0200

    changed setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
 setup(
-    author = 'David Jacobs',
-    author_email = 'david@29.io',
-    description = 'A python library for interacting with the Apple Push Notification Service',
-    download_url = 'https://github.com/djacobs/PyAPNs',
+    author = 'Moroz Ilya',
+    author_email = 'flamewowilia@gmail.com',
+    description = 'Modification of python library for interacting with the Apple Push Notification Service',
+    download_url = 'https://github.com/flamewow/PyAPNs.git',
     license = 'unlicense.org',
-    name = 'apns',
-    py_modules = ['apns'],
+    name = 'apns_py2py3',
+    py_modules = ['apns_py2py3'],
     scripts = ['apns-send'],
     url = 'http://29.io/',
     version = '2.0.1',
